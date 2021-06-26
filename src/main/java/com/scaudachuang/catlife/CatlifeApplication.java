e06dfb98e953ab058d46ee5dff7c644c8dd36a6b
luyunxi <luyunxi@huya.com>
2021-06-26 16:55:45 +0800

    Initial commit

---
+++ src/main/java/com/scaudachuang/catlife/CatlifeApplication.java
@@ -0,0 +1,13 @@
+package com.scaudachuang.catlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatlifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatlifeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scaudachuang/catlife/CatlifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaudachuang.catlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatlifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
